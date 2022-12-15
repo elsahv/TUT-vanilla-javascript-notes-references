8ce360cb4c539cfb51690ed03f01dd5be8f31392
Elsa <elsahvy@gmail.com>
2022-12-15 11:21:14 -0800

    initial commit

---
+++ numbers.js
@@ -0,0 +1,21 @@
+// *NUMBERS
+
+//* an integer is a whole number
+// const myNumber = 42;
+// console.log(myNumber);
+
+//* a number with a decimal point is a float which references the "floating pt"
+// const myFloat = 42.02;
+// console.log(myFloat);
+
+//*NUMBER METHODS- the Number.isInteger() method determines whether the passed value is an integer
+// const myNumber = 42;
+// console.log(Number.isInteger(myNumber));
+
+//* The Number.parseFloat() method parses an arguement and returns a floating point number. If a number cannot be parsed from the arguement, it returns NaN.
+// const myFloat = 42.02;
+// console.log(Number.parseFloat(myFloat));
+
+//* The toFixed() method formats a number according to how many decimal points you provide as a parameter.
+// const myFloat = 42.02;
+// console.log(myFloat.toFixed(2));
---
+++ strings.js
@@ -0,0 +1,38 @@
+//* LENGTH PROPERTY
+// const variable = "math";
+// console.log(variable.length);
+
+// console.log("string example".length);
+
+//* CHARACTER AT METHOD- returns character position number
+// const myVariable = "mathmatics";
+// console.log(myVariable.charAt(4));
+
+//* CHARACTER AT METHOD- returns index character position number
+// const myVariable = "Mathmatics";
+// console.log(myVariable.indexOf("tics"));
+
+//* CHARACTER AT METHOD- returns last index character position number (use if repeated letters)
+// const myVariable = "Mathmatics";
+// console.log(myVariable.lastIndexOf("at"));
+
+//* SLICE METHOD
+// const myVariable = "Mathmatics";
+// console.log(myVariable.slice(5));
+
+//* UPPERCASE/ LOWERCASE METHOD
+// const myVariable = "Mathmatics";
+// console.log(myVariable.toUpperCase());
+// console.log(myVariable.toLowerCase());
+
+//* INCLUDES METHOD- returns boolean
+// const myVariable = "Mathmatics";
+// console.log(myVariable.includes("Math"));
+
+//* SPLIT METHOD- returns array
+const myVariable = "Mathmatics";
+console.log(myVariable.split("h"));
+console.log(myVariable.split(""));
+console.log("John,Joe,Dave".split(","));
+console.log("every good boy does fine".split(""));
+console.log("every good boy does fine".split(" "));
